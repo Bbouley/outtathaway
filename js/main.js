810adeffdbb5f2c6fd7face2336380ba692631f2
bbouley <b.bouley@hotmail.co.uk>
2015-08-01 16:06:56 -0600

    added left and right movement across x axis

--- js/main.js
+++ js/main.js
@@ -7,36 +7,40 @@ $(document).on('ready', function() {
 
   var $gameWindow = $('.gameWindow');
   var $playerBox = $('.playerBox');
-  console.log($playerBox.position());
 
   var keyPressed = 0;
 
-  var playerPosition = {top:0, left:15};
+  var $startingPosition = $playerBox.offset().left;
+  console.log($startingPosition);
 
-  $(document).keydown(function(event){
+  var $currentPosition = $startingPosition;
+
+  var moveDistance = 10;
+
+  $(document).on('keydown', function(event){
     keyPressed = event.keyCode;
     console.log(keyPressed);
     moveBox();
-    newPosition();
-    $playerBox.css(playerPosition);
   });
 
-  $(document).keyup(function(event){
+  $(document).on('keyup', function(event){
     keyPressed = 0;
   });
 
 
   function moveBox(){
     if (keyPressed === 37){
-      $playerBox.animate({'left': '-=1px'},'1');
-      keyPressed = 0;
+        var moveLeft = $currentPosition - moveDistance;
+        $playerBox.offset({left: moveLeft});
+        $currentPosition = $playerBox.offset().left;
+        console.log($currentPosition);
     } else if (keyPressed === 39){
-      $playerBox.animate({'right': '-=1px'}, '1');
-      keyPressed = 0;
-    } else {
-      console.log('test');
+       var moveRight = $currentPosition + moveDistance;
+        $playerBox.offset({left: moveRight});
+        $currentPosition = $playerBox.offset().left;
+        console.log($currentPosition);
     }
-  }
+  };
 
   function newPosition(){
     playerPosition = $playerBox.position();
